c2b2f72a54404f7e9817d8c5f7b15ee05b92f582
jmorenoldl <jmorenol37@educastillalamancha.es>
2023-09-13 13:45:20 +0200

    prueba
    funcional

---
+++ prueba/src/main/java/com/mycompany/prueba/Prueba.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba;
+
+import java.io.File;
+
+/**
+ *
+ * @author b15-10m
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+       File ficheroPrueba = new File(".\\ficheroprueba.txt");
+    
+        System.out.println("El tamaño del fichero es:"+ficheroPrueba.length());
+        
+    
+    }
+}
